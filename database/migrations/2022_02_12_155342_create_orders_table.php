2fc428e546263defffe8cfe5b4a40b0f145aca94
mmabdelgawad <mahmoud.abdelgawad@izam.co>
2022-02-13 20:42:54 +0200

    table structure & models & factories

---
+++ database/migrations/2022_02_12_155342_create_orders_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use App\Models\OrderStatus;
+use App\Models\Payment;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(User::class);
+            $table->foreignIdFor(Payment::class);
+            $table->foreignIdFor(OrderStatus::class);
+            $table->uuid('uuid');
+            $table->json('products');
+            $table->json('address');
+            $table->float('delivery_fee')->nullable();
+            $table->float('amount');
+            $table->timestamp('shipped_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
