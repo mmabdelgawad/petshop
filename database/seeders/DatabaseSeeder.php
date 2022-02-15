d7475ea2548af376605e5d964ef2c2c8e85fd960
mmabdelgawad <mahmoud.abdelgawad@izam.co>
2022-02-15 17:57:46 +0200

    update user factory and seed some users

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +14,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        // seed new users
+        User::factory(1)->create([
+            'first_name' => 'petshop',
+            'last_name' => 'buckhill',
+            'email' => 'petshop@buckhill.com',
+            'is_admin' => 0,
+            'email_verified_at' => now(),
+            'last_login_at' => null,
+            'is_marketing' => 0,
+        ]);
+        User::factory(9)->create();
+
     }
 }
